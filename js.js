ee90760e4ad78fed6ead54fc64afe4e45997061a
yangruoyang <yangruoyang@hotmail.com>
2017-03-03 12:26:25 +0800

    e
    e

---
+++ js.js
@@ -0,0 +1,14 @@
+//file:test.js
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('/tmp/1.db',function() {
+  db.run("create table test(name varchar(15))",function(){
+    db.run("insert into test values('hello,world')",function(){
+      db.all("select * from test",function(err,res){
+        if(!err)
+          console.log(JSON.stringify(res));
+        else
+          console.log(err);
+      });
+    })
+  });
+});
